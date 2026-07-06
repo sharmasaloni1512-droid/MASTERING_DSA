3bfb55b77eb47bdc7e99cfe2ee104f7e3ccd8d69
Saloni Sharma <sharmasaloni1512@gmail.com>
2026-07-06 21:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (60.13%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -3,7 +3,6 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         //Approach 1   
         int n = intervals.size();
-        vector<vector<int> > result;
 
         auto lambda = [](vector<int>& vec1, vector<int>& vec2){
             if(vec1[0] == vec2[0]){
@@ -14,16 +13,17 @@ public:
 
         //Custon sorting 
         sort(intervals.begin(), intervals.end(), lambda);
-
-        result.push_back(intervals[0]);
-
+        int lastIndex = intervals[0][1];
+        int cnt = 1;
+        
         for(int i = 1; i < n; i++){
-            if(result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1]){
+            if(lastIndex >= intervals[i][1]){
                 continue;
             }  
-            result.push_back(intervals[i]);    
+            lastIndex = intervals[i][1]; 
+            cnt++;  
         }
 
-        return result.size();
+        return cnt;
     }
 };
