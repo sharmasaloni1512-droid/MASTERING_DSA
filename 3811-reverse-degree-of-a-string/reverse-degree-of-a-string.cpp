9a32ac6cc19dfcac12639b5b04026eb2dcbf9ec2
Saloni Sharma <sharmasaloni1512@gmail.com>
2026-09-23 14:58:51 +0530

    Time: 3 ms (16.17%) | Memory: 9.8 MB (45.23%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        unordered_map<char, int> mpp;
-        int count = 1;
-        for(char ch = 'z'; ch >= 'a'; ch--){
-            mpp[ch] = count;
-            count++;
-        }
-
         int degree = 0;
         int n = s.size();
 
         for(int i = 0; i < n; i++){
             int prod = 1;
-            prod = mpp[s[i]] * (i + 1);
+            prod = (26 - (s[i] - 'a')) * (i + 1);
             degree += prod;
         }
+
+        // cout << 26 - (s[1] - 'a');
         
         return degree;
     }
